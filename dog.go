f4df6ef07bd4539d4f3e09ddb7ad0ef5fe784a2d
P <lpcsc2024@gmail.com>
2024-06-10 14:25:50 +0530

    second

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func WhenGrownUp(s string) string {
+	return "when the puppy grown up if says" + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Loki-git-hub/dog
+
+go 1.22.4
